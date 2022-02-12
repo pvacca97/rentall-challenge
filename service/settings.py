4a3ed5e931922fbc2e1af81d4b13829c0c19607c
patrick.vacca <patrick.vacca@mbp.local>
2022-02-11 20:01:20 -0500

    setup

--- service/settings.py
+++ service/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-21n^(cnpdk=d-l(z$r3biqx@ybod0mk&ab4()^$b2axpr%-o79'
+SECRET_KEY = 'django-insecure--585i*6aqg2(%a@f4^ifr8xizhtsik73!&1v6c(_9gz+b7a#y8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,7 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework',
 ]
 
 MIDDLEWARE = [
