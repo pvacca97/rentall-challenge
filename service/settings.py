ca9f0cc4bd9dceca666e1b197b29ed0cd98213dc
patrick.vacca <patrick.vacca@mbp.local>
2022-02-17 20:23:21 -0500

    added django-heroku

--- service/settings.py
+++ service/settings.py
@@ -10,7 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
+import django_heroku
 from pathlib import Path
+
+django_heroku.settings(locals())
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -24,7 +27,7 @@ STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--585i*6aqg2(%a@f4^ifr8xizhtsik73!&1v6c(_9gz+b7a#y8'
+SECRET_KEY = os.getenv('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
