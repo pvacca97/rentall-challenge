0c219774675ca7eb7015ac6217161d1aaf2cec24
patrick.vacca <patrick.vacca@mbp.local>
2022-02-17 20:33:30 -0500

    mobed django heroku settings to bottom of file

--- service/settings.py
+++ service/settings.py
@@ -13,7 +13,6 @@ import os
 import django_heroku
 from pathlib import Path
 
-django_heroku.settings(locals())
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -166,3 +165,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 MEDIA_URL = '/media/'
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+django_heroku.settings(locals())
